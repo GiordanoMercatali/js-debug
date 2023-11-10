6e8087f813106e1954c392f79b724f1f3122b06e
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-10 16:36:11 +0100

    add: more corrections

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -20,20 +20,21 @@ for (let i = 0; i < 5; i++) { // Change "major" with "minor" symbol, or else it'
 // ESERCIZIO 2: Returns the value of "num" if it is Even, so it's divisible by 2 without returning any. It should also add 5 to the result.
 function addIfEven(num) {
     if (num % 2 === 0) { // We need "===" to compare the two values, since "=" is used to set the first equal to the second.
-        num += 5; // The function will return "num" without the +5
+        return num + 5; // The function will return "num" without the +5
     }
-    console.log("Result of ex.2: " + num);
+    
     return num;
 }
-let number = 4;
-addIfEven(number);
+let number = 4; // Declared an element to use as "num".
 
-number = 5;
-addIfEven(number);
+let exerciseTwo = addIfEven(number);
+
+console.log(exerciseTwo);
+
 
 // ESERCIZIO 3: Prints "i"'s value in console until "i" becomes higher than 5.
 function loopToFive() {
-    for (let i = 0; i < 5; i++) { // We need to change the "commas" with "semicolons".
+    for (let i = 0; i <= 5; i++) { // We need to change the "commas" with "semicolons".
         console.log(i);
     }
 }
@@ -48,8 +49,8 @@ function displayEvenNumbers() {
     let evenNumbers = [];
     for (let i = 0; i < numbers.length; i++) { // The last condition of the for loop must not have the "semicolon", also if we want the "evenNumber" array to contain 8, we don't need the - 1 after numbers.length.
         const curNumber = numbers[i];
-        if (curNumber % 2 === 0); { // We don't compare the entire array, just the value in the "i" position, so I made a curNumber element to contain that value. We need "===" to compare the two values, since "=" is used to set the first equal to the second.
-            evenNumbers.push(numbers[i]); //We don't need "i", but the current element of "numbers" array.
+        if (curNumber % 2 === 0) { // We don't compare the entire array, just the value in the "i" position, so I made a curNumber element to contain that value. We need "===" to compare the two values, since "=" is used to set the first equal to the second. Removed semicolon.
+            evenNumbers.push(curNumber); //We don't need "i", but the current element of "numbers" array. We use curNumber for it.
         }
          //Return goes outside the for loop.
     }
